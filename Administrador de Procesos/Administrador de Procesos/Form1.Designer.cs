5e81be73030311111ab650df2f771997599c5423
DiegoCrispinR <145538386+DiegoCrispinR@users.noreply.github.com>
2023-09-20 01:22:05 -0600

    Primer Commit

---
+++ Administrador de Procesos/Administrador de Procesos/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Administrador_de_Procesos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvAdministrador = new MetroFramework.Controls.MetroGrid();
+            this.dgvProcesos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvVirtual = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvCPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvActualizar = new MetroFramework.Controls.MetroTile();
+            this.dgvDetener = new MetroFramework.Controls.MetroTile();
+            this.dgvSalir = new MetroFramework.Controls.MetroTile();
+            this.txtContador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProceso = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAdministrador
+            // 
+            this.dgvAdministrador.AllowUserToResizeRows = false;
+            this.dgvAdministrador.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvAdministrador.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvAdministrador.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvAdministrador.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdministrador.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvProcesos,
+            this.dgvID,
+            this.dgvFisica,
+            this.dgvVirtual,
+            this.dgvCPU});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAdministrador.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvAdministrador.EnableHeadersVisualStyles = false;
+            this.dgvAdministrador.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgvAdministrador.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.Location = new System.Drawing.Point(410, 63);
+            this.dgvAdministrador.Name = "dgvAdministrador";
+            this.dgvAdministrador.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvAdministrador.RowHeadersWidth = 51;
+            this.dgvAdministrador.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvAdministrador.RowTemplate.Height = 24;
+            this.dgvAdministrador.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAdministrador.Size = new System.Drawing.Size(662, 442);
+            this.dgvAdministrador.Style = MetroFramework.MetroColorStyle.Silver;
+            this.dgvAdministrador.TabIndex = 1;
+            this.dgvAdministrador.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAdministrador_CellContentClick);
+            // 
+            // dgvProcesos
+            // 
+            this.dgvProcesos.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvProcesos.HeaderText = "Procesos";
+            this.dgvProcesos.MinimumWidth = 6;
+            this.dgvProcesos.Name = "dgvProcesos";
+            // 
+            // dgvID
+            // 
+            this.dgvID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvID.HeaderText = "ID";
+            this.dgvID.MinimumWidth = 6;
+            this.dgvID.Name = "dgvID";
+            // 
+            // dgvFisica
+            // 
+            this.dgvFisica.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvFisica.HeaderText = "Memoria Fisica";
+            this.dgvFisica.MinimumWidth = 6;
+            this.dgvFisica.Name = "dgvFisica";
+            // 
+            // dgvVirtual
+            // 
+            this.dgvVirtual.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvVirtual.HeaderText = "Memoria Virtual";
+            this.dgvVirtual.MinimumWidth = 6;
+            this.dgvVirtual.Name = "dgvVirtual";
+            // 
+            // dgvCPU
+            // 
+            this.dgvCPU.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvCPU.HeaderText = "CPU";
+            this.dgvCPU.MinimumWidth = 6;
+            this.dgvCPU.Name = "dgvCPU";
+            // 
+            // dgvActualizar
+            // 
+            this.dgvActualizar.ActiveControl = null;
+            this.dgvActualizar.Location = new System.Drawing.Point(25, 63);
+            this.dgvActualizar.Name = "dgvActualizar";
+            this.dgvActualizar.Size = new System.Drawing.Size(379, 77);
+            this.dgvActualizar.Style = MetroFramework.MetroColorStyle.Orange;
+            this.dgvActualizar.TabIndex = 2;
+            this.dgvActualizar.Text = "ACTUALIZAR PROCESOS";
+            this.dgvActualizar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dgvActualizar.UseSelectable = true;
+            this.dgvActualizar.Click += new System.EventHandler(this.dgvActualizar_Click);
+            // 
+            // dgvDetener
+            // 
+            this.dgvDetener.ActiveControl = null;
+            this.dgvDetener.Location = new System.Drawing.Point(25, 146);
+            this.dgvDetener.Name = "dgvDetener";
+            this.dgvDetener.Size = new System.Drawing.Size(379, 90);
+            this.dgvDetener.Style = MetroFramework.MetroColorStyle.Brown;
+            this.dgvDetener.TabIndex = 3;
+            this.dgvDetener.Text = "DETENER PROCESOS";
+            this.dgvDetener.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dgvDetener.UseSelectable = true;
+            this.dgvDetener.Click += new System.EventHandler(this.dgvDetener_Click);
+            // 
+            // dgvSalir
+            // 
+            this.dgvSalir.ActiveControl = null;
+            this.dgvSalir.Location = new System.Drawing.Point(25, 242);
+            this.dgvSalir.Name = "dgvSalir";
+            this.dgvSalir.Size = new System.Drawing.Size(379, 93);
+            this.dgvSalir.Style = MetroFramework.MetroColorStyle.Silver;
+            this.dgvSalir.TabIndex = 4;
+            this.dgvSalir.Text = "SALIR";
+            this.dgvSalir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dgvSalir.UseSelectable = true;
+            this.dgvSalir.Click += new System.EventHandler(this.dgvSalir_Click);
+            // 
+            // txtContador
+            // 
+            this.txtContador.AutoSize = true;
+            this.txtContador.BackColor = System.Drawing.Color.Gold;
+            this.txtContador.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtContador.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContador.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtContador.Location = new System.Drawing.Point(25, 564);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.Size = new System.Drawing.Size(82, 26);
+            this.txtContador.TabIndex = 5;
+            this.txtContador.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(514, 564);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre Del Proceso";
+            // 
+            // txtProceso
+            // 
+            this.txtProceso.AutoSize = true;
+            this.txtProceso.BackColor = System.Drawing.Color.Gold;
+            this.txtProceso.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtProceso.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProceso.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtProceso.Location = new System.Drawing.Point(761, 564);
+            this.txtProceso.Name = "txtProceso";
+            this.txtProceso.Size = new System.Drawing.Size(23, 26);
+            this.txtProceso.TabIndex = 7;
+            this.txtProceso.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 636);
+            this.Controls.Add(this.txtProceso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContador);
+            this.Controls.Add(this.dgvSalir);
+            this.Controls.Add(this.dgvDetener);
+            this.Controls.Add(this.dgvAdministrador);
+            this.Controls.Add(this.dgvActualizar);
+            this.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.Text = "ADMINISTRADOR DE PROCESOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroGrid dgvAdministrador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvProcesos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvFisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvVirtual;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvCPU;
+        private MetroFramework.Controls.MetroTile dgvActualizar;
+        private MetroFramework.Controls.MetroTile dgvDetener;
+        private MetroFramework.Controls.MetroTile dgvSalir;
+        private System.Windows.Forms.Label txtContador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtProceso;
+    }
+}
+
